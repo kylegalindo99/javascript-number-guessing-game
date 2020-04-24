facca01ebe838627ff9e5549e3e7400d2b9fb2aa
kgalindo666 <32023592+kgalindo666@users.noreply.github.com>
2020-04-24 14:58:55 -0700

    checkGuess function made

--- file.js
+++ file.js
@@ -1,20 +1,51 @@
-
 // get random number from 1-100
-let randomNumber = Math.floor(Math.random() * 100) +1
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-// reference to parts of our UI // 
-const guesses = document.querySelector('.guesses')
-const lastResult = document.querySelector('.lastResult')
-const lowOrHi = document.querySelector('.lowOrHi')
+// reference to parts of our UI //
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-const guessField = document.querySelector('.guessField')
-const guessSubmit = document.querySelector('.guessSubmit')
+const guessField = document.querySelector(".guessField");
+const guessSubmit = document.querySelector(".guessSubmit");
 
-
-// variables
+// VARIABLES
 let guessCount = 1;
 let resetButton;
 
+// FUNCTIONS
+checkGuess = () => {
+  let userGuess = Number(guessField.value);
 
+  if (guessCount === 1) {
+    guesses.textContent = "previous guesses: ";
+  }
+
+  guesses.textContent += userGuess + "";
+
+  if (useGuess === randomNumber) {
+    lastResult.textContent = "Congrats, you got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!! GAME OVER !!!";
+    setGameOver();
+  } else {
+    lastResult.textContent = "WRONG!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Last guess was too high! ";
+    }
+  }
+  guessCount++;
+  guessField.value = "";
+  guessField.focus()
+};
+
+
+// EVENT LISTENERS
 
 
